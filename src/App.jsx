1cd0ca4cfdb924e9ef62e81c4d55075dddc10ed2
Taian Chen <wallenchen0202@gmail.com>
2026-01-18 14:15:17 +0700

    q

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { SettingsProvider, useSettings } from "./contexts"; // 確保路徑指向你的 contexts.jsx
 import Header from "./Components/header";
-import SettingsPanel from "./components/SettingsPanel";
+import SettingsPanel from "./Components/SettingsPanel";
 import PreviewCard from "./Components/PreviewCard";
 
 function AppContent() {
